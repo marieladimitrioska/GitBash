fcaa0664008f7d8e1763fe389734385b9f04d82b
Mariela <marieladimitrioska@gmail.com>
2022-09-21 23:17:15 +0200

    My first change on gitbash repo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello from Git Bash project!");
+        System.out.println("Hello from Git Bash project! GitBash Change");
     }
 }
